9c9b1f2d064b719d81f331be33eee4b18e7c2797
KHALFALLAH Razzak (SNCF / DGA NUMERIQUE / e.SNCF Sol.D2D DDA TP DOI) <9506532m@commun.ad.sncf.fr>
2024-06-19 01:52:55 +0200

    feat(pdw-1): creation of mvc achitecture and basic implementation

--- src/main/java/com/projet3ocr/api/repositories/PictureRepository.java
+++ src/main/java/com/projet3ocr/api/repositories/PictureRepository.java
@@ -1,4 +1,9 @@
 package com.projet3ocr.api.repositories;
 
-public interface PictureRepository {
+import com.projet3ocr.api.models.Picture;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PictureRepository extends JpaRepository<Picture,Long> {
 }

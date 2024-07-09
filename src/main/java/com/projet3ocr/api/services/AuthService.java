3965c6e041e5eef2c9dfef64405938c20df9b694
KHALFALLAH Razzak (SNCF / DGA NUMERIQUE / e.SNCF Sol.D2D DDA TP DOI) <9506532m@commun.ad.sncf.fr>
2024-07-10 00:20:39 +0200

    chore: add tokenConfig for swagger

--- src/main/java/com/projet3ocr/api/services/AuthService.java
+++ src/main/java/com/projet3ocr/api/services/AuthService.java
@@ -3,10 +3,7 @@ package com.projet3ocr.api.services;
 import com.projet3ocr.api.dtos.receiveFromView.LoginUserDto;
 import com.projet3ocr.api.dtos.receiveFromView.RegisterUserDto;
 import com.projet3ocr.api.dtos.sendToView.UserDto;
-import com.projet3ocr.api.models.User;
 
-import java.security.Principal;
-import java.util.HashMap;
 import java.util.Map;
 
 public interface AuthService {
